fe0f9bfc66f6cce41b2b159420236df08ee6b566
chenhs <chenhs@toone.com.cn>
2020-11-23 16:01:34 +0800

    Signed-off-by: chenhs <chenhs@toone.com.cn>

--- src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
+++ src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
@@ -1,14 +1,17 @@
 package org.springframework.samples.mvc.simple;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
-	@GetMapping("/simple")
-	public String simple() {
-		return "Hello world!";
+	@RequestMapping(value = "/simple", produces = "text/event-stream;charset=utf-8")
+	public @ResponseBody String simple() {
+
+		return "retry:500\ndata:Hello world我好呀 !!!!!!!!!!!!!\n\n";
+
 	}
 
 }
